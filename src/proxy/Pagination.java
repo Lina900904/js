ad5b6bb7db01142c84acd576741a555c4fbef7cf
Lee Seul <lina900904@gmail.com>
2018-08-09 09:15:54 +0900

    last proxy

--- src/proxy/Pagination.java
+++ src/proxy/Pagination.java
@@ -12,19 +12,25 @@ public class Pagination implements Proxy {
 
 	public void carryOut(Object o) {
 		this.pageNumber = (int)o;
-		this.pageSize =5;
-		this.blockSize =5;
-		this.rowCount = MemberServiceImpl.getInstance().memberCount();
-		this.pageCount=(rowCount%pageSize ==0)?rowCount/pageSize: rowCount/pageSize +1;
-		this.blockCount=(pageCount%blockSize ==0)?pageCount/blockSize : pageCount/blockSize+1;
-		this.beginPage = pageNumber*pageSize-(pageSize-1);
+		this.pageSize = 5;
+		this.blockSize = 5;
+		this.rowCount =MemberServiceImpl.getInstance().memberCount();
+		this.pageCount = (rowCount%pageSize==0)?
+				rowCount/pageSize : rowCount/pageSize+1;
+		this.blockCount = (pageCount%blockSize==0)?
+				pageCount/blockSize : pageCount/blockSize+1;
+		this.beginRow = pageNumber*pageSize - (pageSize-1);
 		this.endRow = pageNumber*pageSize;
-		this.beginPage= pageNumber-((pageNumber-1)%blockSize);
-		this.endPage= ((beginPage+pageSize-1)<pageCount)?beginPage+blockSize-1:pageCount;
-		this.prevBlock = beginPage-blockSize;
-		this.nextBlock = beginPage+blockSize;
-		this.existPrev = (prevBlock>=0);
-		this.existNext= (nextBlock<=pageCount);
+		this.beginPage = pageNumber -((pageNumber-1)%blockSize);
+		this.endPage = ((beginPage + pageSize -1)<pageCount)?
+				beginPage+blockSize-1:pageCount;
+		this.prevBlock = beginPage - blockSize;
+		this.nextBlock = beginPage + blockSize;
+		this.existPrev = (prevBlock >= 0);
+		System.out.println(">>>>>>>>>>>>>>");
+		System.out.println(">>>"+existPrev);
+		this.existNext =(nextBlock <= pageCount);
+		System.out.println(">>>>"+existNext);
 		
 				
 	}
